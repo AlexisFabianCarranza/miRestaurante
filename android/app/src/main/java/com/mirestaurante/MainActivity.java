a329615e7b78e0b562118378950727e3871d3b55
Alexis Carranza <aleexiz1996@gmail.com>
2019-09-02 01:00:56 -0300

    firebase configurado

---
+++ android/app/src/main/java/com/mirestaurante/MainActivity.java
@@ -0,0 +1,15 @@
+package com.mirestaurante;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "miRestaurante";
+    }
+}
