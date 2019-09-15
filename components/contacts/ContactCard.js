703ed19cfa7fcc97dcbaa9b089e04eb86efd5f06
Alexis Carranza <alexis.carranza@claro.com.ar>
2019-09-15 19:05:15 -0300

    Se soluciona bug de contactos y se agrega color verde en contactos a elegir

--- components/contacts/ContactCard.js
+++ components/contacts/ContactCard.js
@@ -4,19 +4,31 @@ import { Card, Title, IconButton } from 'react-native-paper';
 import styles from '../../styles/home.style';
 
 export default class ContactCard extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            selected: false
+        };
+    }
     render(){
         console.log(this.props.user.name);
         let props = this.props;
         return(
-            <Card style={styles.card}> 
+            <Card style={(this.state.selected) ? {...styles.card, backgroundColor:'green'} : styles.card}> 
                 <Card.Content style={{flexDirection:'row'}}>
                     <Image style={{width:40, height:40}} source={{uri: props.user.avatar}}/>
                     <View style={{flex:1, marginLeft:10}}>
                         <Title>{props.user.name}</Title>
                     </View>
                     {props.addContactToEvent && <IconButton 
+                        style={{flex: 1,width: 40,height:40}}
                         icon='person-add'
-                        onPress={() => props.addContactToEvent(props.user)}
+                        onPress={() => {
+                            this.setState({
+                                selected: true
+                            });
+                            this.props.addContactToEvent(props.user);
+                        }}
                     />}
                 </Card.Content>
             </Card>
